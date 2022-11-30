6f254e79c64dbfd0226e1e961f98ef3bbd9fcab9
Md Abid Sikder <abid@abidsikder.com>
2022-11-30 10:55:26 -0500

    feat: basic green rotating cube

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
 import * as THREE from 'three'
-import BlasterScene from './BlasterScene'
+import { _SRGBAFormat } from 'three'
 
 const width = window.innerWidth
 const height = window.innerHeight
 
+
+// function getShaderFile(textFile: string) {
+//   var request = new XMLHttpRequest();
+//   request.open("GET", textFile, false);
+//   request.overrideMimeType("text/plain");
+//   request.send(null);
+//   return request.responseText;
+// };
+// const vertexSrc = getShaderFile("./frag.glsl")
+// const fragSrc = getShaderFile("./vertex.glsl")
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(50,width/height,0.1,2000)
 const renderer = new THREE.WebGLRenderer({
 	canvas: document.getElementById('app') as HTMLCanvasElement
 })
 renderer.setSize(width, height)
 
-const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
 
-const scene = new BlasterScene(mainCamera)
-scene.initialize()
+scene.add(cube)
+camera.position.z = 5;
+renderer.render(scene,camera)
 
-function tick()
-{
-	scene.update()
-	renderer.render(scene, mainCamera)
-	requestAnimationFrame(tick)
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
 }
+animate();
 
-tick()
