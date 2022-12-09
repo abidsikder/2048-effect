e72babcb0ce81b7bca11b76d69632f8e9bc891b7
Md Abid Sikder <abid@abidsikder.com>
2022-12-09 15:17:33 -0500

    feat: add all colors in an object

---
+++ src/view.ts
@@ -0,0 +1,31 @@
+
+// n is the number
+// function generateBoxTile(n:number) {
+
+// }
+
+// hex colors in strings for all elements in the game
+const colors = {
+    2: "A5E871",
+    4: "7FDA88",
+    8: "2ABA7E",
+    16: "1BCA95",
+    32: "39B9CB",
+    64: "46B9FA",
+    128: "468EFA",
+    256: "7471F0",
+    512: "8F65E9",
+    1024: "D868E1",
+    2048: "DC629D",
+    boardBorder: "00A3FF",
+    titleText: "0B0000",
+    titleGlow: "28D0E6",
+    scoreLabel: "4CC3DD",
+    scoreNumber: "4CC3DD",
+    newGameText: "4DC9E4",
+    newGameButton: "328597",
+    winText: "D7F3EC",
+    winGlow: "28D0E6",
+    gameOverText: "A02626",
+    gameOverGlow: "050505"
+};
